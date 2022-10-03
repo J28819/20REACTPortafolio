bc426c9e2ba69e246c6fa2b19d0db5177ed7bb3b
J28819 <lelj28819@gmail.com>
2022-10-03 15:21:17 -0400

    Adding Img.logo and About me Information, iot.png banner

--- server/src/App.js
+++ server/src/App.js
@@ -1,36 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import Nav from './components/Nav';
 import NavBar from './components/BSNvar';
+import imgoverley from './components/imgoverley';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import ImgOverlayExample from './components/imgoverley';
 
 
 function App() {
   return (
     
     <div className="App">
-     
-      <NavBar />
-      <Nav />
-
-
+        <NavBar />
+        <ImgOverlayExample>
       
+        </ImgOverlayExample>
+        
+   
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to know more about my Projects
         </a>
       </header>
     </div>
--- server/src/components/BSNvar.js
+++ server/src/components/BSNvar.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-function TextLinkExample() {
+function CollapsibleExample() {
   return (
-    <Navbar>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+        <Navbar.Brand href="#home">Ismael leal</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#aboutme">About me </Nav.Link>
+            <Nav.Link href="#Portafolio">Portafolio</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Contact</Nav.Link>
+            <Nav.Link eventKey={2} href="https://www.linkedin.com/in/ismael-leal-45062429/">
+              Resume
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default TextLinkExample;
+export default CollapsibleExample;
