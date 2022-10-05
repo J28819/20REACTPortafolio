3b01a28d64f5e141cd556e570aa376cd14037deb
J28819 <lelj28819@gmail.com>
2022-10-05 13:43:22 -0400

    Addimng Heroku

--- src/components/BSNvar.js
+++ src/components/BSNvar.js
@@ -7,16 +7,16 @@ function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/home">Ismael leal</Navbar.Brand>
+        <Navbar.Brand href="/20REACTPortfolio/home">Ismael leal</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/aboutme">About me </Nav.Link>
-            <Nav.Link href="/portafolio">Portofolio</Nav.Link>
+            <Nav.Link href="/20REACTPortfolio/aboutme">About me </Nav.Link>
+            <Nav.Link href="/20REACTPortfolio/portafolio">Portofolio</Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link  href="/resume">
+            <Nav.Link href="/20REACTPortfolio/contact">Contact</Nav.Link>
+            <Nav.Link  href="/20REACTPortfolio/resume">
               Resume
             </Nav.Link>
           </Nav>
